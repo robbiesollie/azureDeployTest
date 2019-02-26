d54a70d0062939c598608e5eced64fced9cce6bb
Alicia Locker <wiisha.locker@gmail.com>
2019-02-26 14:13:33 -0800

    Merge branch 'developer' of C:\Users\wiish\Desktop\techConnectCBU with conflicts.

--- webapps/ROOT/WEB-INF/classes/techConnect/SolutionProviderDAO.java
+++ webapps/ROOT/WEB-INF/classes/techConnect/SolutionProviderDAO.java
@@ -1,59 +1,51 @@
-package techConnect;
-
-import java.sql.ResultSet;
-import java.util.LinkedList;
-import java.util.Queue;
-
-//Dao fo exclusive solution provider methods
-public class SolutionProviderDAO extends UserDAO {
-
-    //Creates a new solution provider
-    public void setSolutionProvider(providerBean bean) throws java.sql.SQLException {
-        //char userType, String name, String password, String email, String affiliation, String location, Boolean group, Boolean isPrivate
-        if(bean.getUserName() != null && bean.getPassword() != null && bean.getEmail() != null && bean.getAffiliation() != null && bean.getLocation() != null && bean.getGroup() != null) {
-            DB.addUser('S', bean.getUserName(), bean.getPassword(), bean.getEmail(), bean.getAffiliation(), bean.getLocation(), bean.getGroup(), false);
-        }
-    }
-
-    //Returns user data when given the correct name and password
-    public providerBean solutionProviderLogin(providerBean bean) throws java.sql.SQLException {
-        if(bean.getUserName() != null && bean.getPassword() != null) {
-            ResultSet rs = DB.solutionProviderLogin(bean.getUserName(), bean.getPassword());
-            return makeBean(rs);
-        }
-        return null;
-    }
-
-    //Returns all solution providers
-    public Queue<providerBean> getSolutionProvider() throws java.sql.SQLException {
-        ResultSet rs = DB.getSolutionProviders();
-        return makeBeanQueue(rs);
-    }
-
-    //returns a solution provider of a given ID
-    public providerBean getSolutionProviderFromID(providerBean bean) throws java.sql.SQLException {
-        if(bean.getUserID() > 0) {
-            ResultSet rs = DB.getSolutionProviders(bean.getUserID());
-            return makeBean(rs);
-        }
-        return null;
-    }
-
-    //returns a solution provider of a given name
-    public providerBean getSolutionProviderFromName(providerBean bean) throws java.sql.SQLException {
-        if(bean.getUserName() != null) {
-            ResultSet rs = DB.getSolutionProviders(bean.getUserName());
-            return makeBean(rs);
-        }
-        return null;
-    }
-
-
-    public Queue<projectBean> getProjects(providerBean bean) throws java.sql.SQLException {
-        if(bean.getUserID() > 0) {
-            ResultSet rs = DB.getSolutionProvidersProjects(bean.getUserID());
-            return makeProjectBeanQueue(rs);
-        }
-        return null;
-    }
-}
+//package techConnect;
+//import java.sql.*;
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
+//
+//public class SolutionProviderDAO {
+//    /**
+//     * Initialize a SolutionProvider with the given bean containing Skill information and username,
+//     * return true if success
+//     */
+////    public static boolean initProvider(SolutionProviderBean bean) {
+////        //TODO: Implement DAO to make database calls @MCecil @ALocker
+////        throw new RuntimeException("Method has not yet been implemented");
+////    }
+//    private DBWrapper DB;
+//
+//    public SolutionProviderDAO(){
+//        DB = new DBWrapper();
+//    }
+//
+//    public Queue<SolutionProviderBean> getUsername() throws java.sql.SQLException{
+//        ResultSet rs = DB.getSolutionProviders();
+//        return makeBeanQueue(rs);
+//    }
+//
+//    public Queue<SolutionProviderBean> getSkills() throws java.sql.SQLException{
+//        ResultSet rs = DB.getSkills();
+//        return makeBeanQueue(rs);
+//    }
+//
+//    public void setSkills(SolutionProviderBean bean) throws java.sql.SQLException{
+//        if(bean.getSkills() != null){
+//            DB.addSkill("skillName");
+//        }
+//    }
+//    private Queue<SolutionProviderBean> makeBeanQueue(ResultSet rs) throws java.sql.SQLException {
+//        Queue<SolutionProviderBean> beanSet = new LinkedList<>();
+//        do {
+//            beanSet.add(makeBean(rs));
+//        }while(rs.next());
+//        rs.close();
+//        return beanSet;
+//    }
+//
+//    private publicProjectBean makeBean(ResultSet rs) throws java.sql.SQLException{
+//        SolutionProviderBean bean = new SolutionProviderBean();
+//
+//        return bean;
+//    }
+//}
